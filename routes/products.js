2d73e73d01395dd9c5be2af14440a6d5527238b7
Juliana Muñoz Marin <jmmalcg@gmail.com>
2023-11-14 23:21:29 -0500

    Add API to get product by Id.

--- routes/products.js
+++ routes/products.js
@@ -6,9 +6,20 @@ const productsService = new ProductsService();
 
 router.get('/', async function (req, res, next) {
   try {
-    res.status(200).send(await productsService.getProducts(req.query))
+    const response = await productsService.getProducts(req.query);
+    res.status(response.code).send(response.data);
   } catch (error) {
-    console.error('Error in: GET /products ', error)
+    console.error('Error in: GET /products ', error);
+    res.status(500).send('An error has occurred, try later.');
+  }
+});
+
+router.get('/:productId', async function (req, res, next) {
+  try {
+    const response = await productsService.getProductById(req.params.productId);
+    res.status(response.code).send(response.data);
+  } catch (error) {
+    console.error('Error in: GET /:productId ', error)
     res.status(500).send('An error has occurred, try later.');
   }
 });
--- services/products.service.js
+++ services/products.service.js
@@ -1,17 +1,22 @@
 import fetch from 'node-fetch';
 
 class ProductsService {
+  baseUrl;
+
   constructor() {
+    this.baseUrl = 'https://dummyjson.com/products/'
   }
 
   async getProducts(params) {
-    const baseUrl = 'https://dummyjson.com/products?limit=10';
+    const productsUrl = `${this.baseUrl}?limit=10`;
 
-    const res = await fetch(this.addUrlParameters(baseUrl, params));
+    const res = await fetch(this.addUrlParameters(productsUrl, params));
     return {
       code: 200,
-      message: 'Data found',
-      data: await res.json()
+      data: {
+        message: 'Data found',
+        data: await res.json()
+      }
     };
   };
 
@@ -32,6 +37,32 @@ class ProductsService {
 
     return url
   }
+
+  async getProductById(productId) {
+    if (productId) {
+      const productIdUrl = `${this.baseUrl}/${productId}`;
+
+      const res = await fetch(productIdUrl);
+
+      if (res.status == 200) {
+        return {
+          code: 200,
+          data: {
+            message: 'Product found',
+            data: await res.json()
+          }
+        };
+      }
+    }
+
+    return {
+      code: 404,
+      data: {
+        message: 'Product not found',
+        data: null
+      }
+    };
+  };
 }
 
 export default ProductsService;
